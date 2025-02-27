2d902a4f98be971afdb211455daffb69ec4d7b9e
Muhammad Haikal Bintang <mhaikalbintang.work@gmail.com>
2025-02-27 23:42:40 +0700

    Create Surah -mrc

--- database/migrations/2025_02_27_163223_create_surahs_table.php
+++ database/migrations/2025_02_27_163223_create_surahs_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('surahs', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
+
+            $table->integer('number');
             $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
+            $table->integer('total_ayahs');
+
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('surahs');
     }
 };
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\SurahController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('surahs', SurahController::class)->only(['index', 'show']);
